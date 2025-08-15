b40d06cea4f82bf8e2c07c5c93ea30d4e8997743
Rabby404F <eyasin.bhuiyan.404@gmail.com>
2025-08-15 19:42:06 +0600

    sol primary tasks

---
+++ A_Primary_Task.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+  string s;
+  cin>>s;
+
+  char a='1';
+  char  b='0';
+  int count=0;
+
+  if(s[0]!=a || s[1]!=b)
+    {count++;}
+  
+    if(s[2]=='0' )count++;
+    if(s.length()==3 && s[2]=='1')
+    {
+        count++;
+    }
+
+  
+ if(count==0)cout<<"Yes"<<endl;
+ else cout<<"No"<<endl;
+    }
+}

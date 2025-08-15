224cdfc0a8c81dc7cbfdfa9aa8043ee3cdd0ce42
Rabby404F <eyasin.bhuiyan.404@gmail.com>
2025-08-15 13:20:44 +0600

    beautiful matrix sol

--- beautiful_matrix.cpp
+++ beautiful_matrix.cpp
@@ -1,27 +1,24 @@
 #include<iostream>
+#include<string>
+#include<cmath>
 using namespace std;
-int main(){
 
-  int arr[5][5];
-  
+int main()
+{  int t;
+  cin>>t;
+  while(t--)
+  {
+    int x;
+    cin>>x;
+  string s;
+  cin>>s;
+  int count=0;
+  int n= s.length();
+  double r=sqrt(n);
 
-  for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-      cin>>arr[i][j];
-    }
+  for(auto u:s) if(u=='0')count++;
+
+  if(count==(r-2)*(r-2))cout<<"Yes"<<endl;
+  else {cout<<"No"<<endl;}
   }
-  int x,y;
-  for(int i=0;i<5;i++){
-    for(int j=0;j<5;j++){
-      if(arr[i][j]==1)
-      {x=i;
-      y=j;}
-    }
-  }
-   
-  int sum=abs(x-2)+abs(y-2);
-  cout<<sum<<endl;
-
-  return 0;
-  
 }

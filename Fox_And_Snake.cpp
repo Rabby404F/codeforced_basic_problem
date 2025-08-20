f666f73656d4b641dad1cdf6e70d8517ecb9e667
Rabby404F <eyasin.bhuiyan.404@gmail.com>
2025-08-20 17:33:14 +0600

    fox_and_snakes_soln

---
+++ Fox_And_Snake.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#include<string>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+    string s;
+    
+    for(int j=0;j<n;j++)
+    {
+        if(j==n-1){s.push_back('#');}
+        else{s.push_back('.');}
+    }
+    cout<<s<<endl;
+    for(int i=0;i<m;i++)
+    {
+        if(i%2==0)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cout<<"#";
+            }
+            cout<<endl;
+        }
+        else{
+            cout<<s<<endl;
+            reverse(s.begin(),s.end());
+        }
+        
+        }
+    }
